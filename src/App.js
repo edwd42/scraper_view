92f71022bb5cd855970e0be7065844c4de48adfe
edwd42 <edwd42@yahoo.com>
2019-08-20 08:23:08 -0400

    add src/Components/Watchlist.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
+import Watchlist from './Components/Watchlist';
 
 function App() {
   return (
     <div className="App">
-		<h1>I'm a teapot</h1>
+		<h1>CareerDevs Capstone Scraper Project - MVP</h1>
+		<Watchlist/>
     </div>
   );
 }
---
+++ src/Components/Watchlist.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const watchlist = () => {
+	return <p>I'm a little teapot!</p>
+}
+
+export default watchlist;
