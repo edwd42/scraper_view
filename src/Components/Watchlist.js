aff6c6da47cf85e8e82ab94c9bcba6b973a5ca98
edwd42 <edwd42@yahoo.com>
2019-08-23 23:44:20 -0400

    add button and click handler

--- src/Components/Watchlist.js
+++ src/Components/Watchlist.js
@@ -1,27 +1,43 @@
 import React, { Component } from 'react'
+import $ from 'jquery';
 
 class Watchlist extends Component {
 
-    constructor(props) {
-		super(props);
+    constructor() {
+		super()
 		this.state = {
 			watchlist:[]
-		}
+        }
+        this.handleClick = this.handleClick.bind(this)
     }
     
     componentDidMount(){
-        fetch("http://localhost:8081/rest/api/findLastScrape")
-            .then(response => response.json())
-            .then(response => {
-                const {watchlist} = response.data
-                console.log(watchlist[0])
-                this.setState({ watchlist: watchlist })
-            })
+
+        $.ajax({
+            url: 'http://localhost:8081/rest/api/findLastScrape/',
+            dataType:'json',
+            cache: false,
+            success: function(data){
+				console.log(data);
+                this.setState({watchlist: data});
+            }.bind(this),
+            error: function(xhr, status, err){
+                console.log("err " + err);
+                console.log("status " + status);
+                console.log("xhr.response " + xhr.responseText);
+            }
+        });
     }
 
+    handleClick(event){
+        console.log("working!!!")
+    }
     render(){
         return (
-            <h3>Watchlist</h3>
+            <div>
+                <h3>Watchlist</h3>
+                <button onClick={this.handleClick}>New Scrape</button>
+            </div>
         )
     }
 }
