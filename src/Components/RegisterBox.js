8ca76380b5bcb6c9d5a57ffa8067c8248d3f8692
edwd42 <edwd42@yahoo.com>
2019-08-25 18:46:21 -0400

    add form validation

--- src/Components/RegisterBox.js
+++ src/Components/RegisterBox.js
@@ -4,20 +4,40 @@ import './styles/_loginSty.scss'
 
 export default class RegisterBox extends React.Component {
 
-	constructor(props) {
-	super(props);
-	this.state = {};
-	}
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: "",
+      email: "",
+      password: "",
+      errors: [],
+      pwdState: null
+    };
+  }
 
-	submitRegister(e) {}
+  submitRegister(e) {
+
+    console.log(this.state);
+
+    if (this.state.username == "") {
+      this.showValidationErr("username", "Username Cannot be empty!");
+    }
+    if (this.state.email == "") {
+      this.showValidationErr("email", "Email Cannot be empty!");
+    }
+    if (this.state.password == "") {
+      this.showValidationErr("password", "Password Cannot be empty!");
+    }
+  }
+
 
 	showValidationErr(elm, msg) {
 		this.setState((prevState) => ({
 			errors: [
-			...prevState.errors, {
-				elm,
-				msg
-			}
+				...prevState.errors, {
+					elm,
+					msg
+				}
 			]
 		}));
 	}
@@ -26,69 +46,109 @@ export default class RegisterBox extends React.Component {
 		this.setState((prevState) => {
 			let newArr = [];
 			for (let err of prevState.errors) {
-			if (elm != err.elm) {
-				newArr.push(err);
+				if (elm != err.elm) {
+					newArr.push(err);
+				}
 			}
-			}
-			return {errors: newArr};
+			return { errors: newArr };
 		});
 	}
 
 	//Update Username, password, and email on change event 
 	onUsernameChange(e) {
-		this.setState({username: e.target.value});
+		this.setState({ username: e.target.value });
 		//We want to clear the error when ever the user type something new 
 		this.clearValidationErr("username");
 	}
 
 	onEmailChange(e) {
-		this.setState({email: e.target.value});
+		this.setState({ email: e.target.value });
 		this.clearValidationErr("email");
 	}
 
 	onPasswordChange(e) {
-		this.setState({password: e.target.value});
+		this.setState({ password: e.target.value });
 		this.clearValidationErr("password");
 	}
 
-  render() {
-    return (
-      <div className="inner-container">
-        <div className="header">
-          Register
+	render() {
+
+		let usernameErr = null,
+			passwordErr = null,
+			emailErr = null;
+
+		for (let err of this.state.errors) {
+			if (err.elm == "username") {
+				usernameErr = err.msg;
+			}
+			if (err.elm == "password") {
+				passwordErr = err.msg;
+			}
+			if (err.elm == "email") {
+				emailErr = err.msg;
+			}
+		}
+
+
+		return (
+			<div className="inner-container">
+				<div className="header">
+					Register
         </div>
-        <div className="box">
+				<div className="box">
 
-          <div className="input-group">
-            <label htmlFor="username">Username</label>
-            <input
-              type="text"
-              name="username"
-              className="login-input"
-              placeholder="Username"/>
-          </div>
+					<div className="input-group">
+						<label htmlFor="username">Username</label>
+						<input
+							type="text"
+							name="username"
+							className="login-input"
+							placeholder="Username"
+							onChange={this
+								.onUsernameChange
+								.bind(this)} />
+						<small className="danger-error">{usernameErr
+							? usernameErr
+							: ""}</small>
+					</div>
 
-          <div className="input-group">
-            <label htmlFor="email">Email</label>
-            <input type="text" name="email" className="login-input" placeholder="Email"/>
-          </div>
+					<div className="input-group">
+						<label htmlFor="email">Email</label>
+						<input
+							type="text"
+							name="email"
+							className="login-input"
+							placeholder="Email"
+							onChange={this
+								.onEmailChange
+								.bind(this)} />
+						<small className="danger-error">{emailErr
+							? emailErr
+							: ""}</small>
+					</div>
 
-          <div className="input-group">
+         <div className="input-group">
             <label htmlFor="password">Password</label>
             <input
               type="password"
               name="password"
               className="login-input"
-              placeholder="Password"/>
-          </div>
-          <button
-            type="button"
-            className="login-btn"
-            onClick={this
-            .submitRegister
-            .bind(this)}>Register</button>
-        </div>
-      </div>
-    );
-  }
+              placeholder="Password"
+              onChange={this
+              .onPasswordChange
+              .bind(this)}/>
+            <small className="danger-error">{passwordErr
+                ? passwordErr
+                : ""}</small>
+					</div>
+					<button
+						type="button"
+						className="login-btn"
+						onClick={this
+							.submitRegister
+							.bind(this)}>Register</button>
+				</div>
+			</div>
+		);
+	}
 }
