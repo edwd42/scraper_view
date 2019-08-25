9ceb57a25b4dc0cfd2b9cd02e8eafbdb223603af
edwd42 <edwd42@yahoo.com>
2019-08-25 17:12:07 -0400

    add class component Login_Register

--- src/App.js
+++ src/App.js
@@ -2,14 +2,16 @@ import React, { Component } from 'react'
 import './App.css'
 import Header from './components/Header'
 import Watchlist from './components/Watchlist'
-
+import Login_Register from './components/Login_Register'
 
 export default class App extends Component {
 
-  constructor(){
-    super()
+  constructor(props){
+    super(props)
     this.state = {
-      isLoggedIn: true
+      isLoggedIn: false,
+      isLoginOpen: true,
+      isRegisterOpen: false
     }
   }
 
@@ -17,6 +19,15 @@ export default class App extends Component {
     let status
     this.state.isLoggedIn ? status = "in" : status = "out"
 
+    if (!this.state.isLoggedIn){
+      return (
+        <div className="App">
+          <Header/>
+          <h1>You are currently logged { status }</h1>
+          <Login_Register/>
+      </div>
+      )
+    }
     return (
       <div className="App">
         <Header/>
---
+++ src/Components/Login_Register.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+import reactDOM from 'react-dom'
+
+export default class Login_Register extends Component{
+
+	 constructor(props) {
+    super(props);
+    this.state = {}
+  }
+
+    render() {
+
+		return (
+			<h1>Hello, please login or register</h1>
+		)
+	}
+}
