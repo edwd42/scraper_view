9d7974107e91734ddb3b22373e454879e2192319
edwd42 <edwd42@yahoo.com>
2019-08-27 21:36:50 -0400

    add components/protectedroute.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import './styles/index.css';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { LandingPage } from "./components/landingpage";
+import { AppLayout } from "./components/applayout";
+import { ProtectedRoute } from "./components/protectedroute";
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <Switch>
+        <Route exact path="/" component={LandingPage} />
+        <ProtectedRoute exact path="/app" component={AppLayout} />
+        <Route path="*" component={() => "404 NOT FOUND"} />
+      </Switch>
+    </div>
+  );
+}
+
+ReactDOM.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+  document.getElementById("root")
+);
+
+// ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
