7c827f49f48dc8d0e1f25f4ddbc430fe0e5b2131
edwd42 <edwd42@yahoo.com>
2019-09-05 14:45:58 -0400

    edit About page

--- src/Components/About.js
+++ src/Components/About.js
@@ -1,25 +1,59 @@
 import React from "react";
 import Header from "./Header";
+import "../assets/styles/App.css";
+
+import SpringBootStack from "../assets/images/SpringBootStack.png";
+import reactjs_logo from "../assets/images/reactjs_logo.png";
+import java_logo from "../assets/images/java-logo.png";
+import spring_logo from "../assets/images/spring-logo.png";
+import mongodb_logo from "../assets/images/mongodb_logo.png";
+
+const spanStyle = {
+  fontWeight: "bold"
+};
 
 function about() {
   return (
-    <div>
+    <>
       <Header />
       <p>About This Project</p>
       <div>
-        <img src={"../assets/images/java-logo.png"} alt="java logo" />
+        <article>
+          <h1>CareerDevs Capstone Scraper Project</h1>
+          <p>
+            <span style={spanStyle}>Main Task </span>Build a Java scraper that
+            will pull stock data from your personal Yahoo finance account. This
+            application should store each scrape in a database so as to be able
+            to retain a history of past scraped data. This scraper will include
+            a web-based user facing interface, which pulls data from a RESTAPI
+            as JSON data, where the user will be able to view the historic data,
+            request a new scrape, and see the most recent scraped data as well.
+          </p>
+
+          <p>
+            Following the MVC pattern (Model-View-Controller) I built this app
+            using Java and Spring Boot for the controller, ReactJS for the view
+            and MongoDB on the back-end. The diagram below depicts controller
+            and database layers.
+          </p>
+          <p>
+            I chose to write the view in ReactJS because it is fast, lightweight
+            and portable.
+          </p>
+        </article>
       </div>
-      {/* <div><img src="${pageContext.request.contextPath}/resources/images/spring-logo.png"></div> */}
-      {/* <div><img src="${pageContext.request.contextPath}/resources/images/hibernatelogo.png"></div> */}
-      {/* <div><img src="${pageContext.request.contextPath}/resources/images/mysql-logo.png"></div> */}
-      {/* <div><img src="${pageContext.request.contextPath}/resources/images/TDD_logo.png"></div> */}
-      {/* <div><img src="${pageContext.request.contextPath}/resources/images/aws-logo.png"></div> */}
-      {/* <div><img src="${pageContext.request.contextPath}/resources/images/cssgrid-logo.png"></div> */}
-      {/* <div><img src="${pageContext.request.contextPath}/resources/images/d3js.png"></div> */}
-      {/* <div><img src="${pageContext.request.contextPath}/resources/images/nodejs-logo.png"></div> */}
-      {/* <div><img src="${pageContext.request.contextPath}/resources/images/jstl-jsp-logo.png"></div> */}
-      {/* <div><img src="${pageContext.request.contextPath}/resources/images/github-logo.png"></div> */}
-    </div>
+      <div>
+        <img src={SpringBootStack} alt="Spring Boot Stack"></img>
+        <img src={reactjs_logo} alt="reactjs logo"></img>
+        <img src={java_logo} alt="java logo" />
+        <img src={spring_logo} alt="spring logo"></img>
+        <img
+          src={mongodb_logo}
+          alt="mongodb logo"
+          style={{ height: "50%", width: "35%" }}
+        ></img>
+      </div>
+    </>
   );
 }
 
--- src/Components/Dashboard.js
+++ src/Components/Dashboard.js
@@ -1,54 +1,56 @@
 // @ts-nocheck
-import React, { createContext } from 'react'
-import '../styles/App.css'
-import { AppContext } from "../App"
+import React, { createContext } from "react";
+import "../assets/styles/App.css";
+import { AppContext } from "../App";
 
 const { Consumer } = createContext({
-	user: {
-		firstname: "Ed",
-	}
-})
+  user: {
+    firstname: "Ed"
+  }
+});
 
 const Profile = () => (
-	<Consumer>
-		{({ user }) => (
-			<span>Hello, {user.firstname}</span>
-		)}
-	</Consumer>
-)
+  <Consumer>{({ user }) => <span>Hello, {user.firstname}</span>}</Consumer>
+);
 
 export const Dashboard = props => (
-	<AppContext.Consumer>
-		{data => (
-
-			< React.Fragment >
-				<div>
-					<div><br /><Profile /></div>
-					<div>
-						<p>
-							<button className="button" onClick={data.handleClickNewScrape} disabled={data.loading}>
-								{data.loading && (
-									<i
-										className="fas fa-sync"
-										style={{ marginRight: "5px" }}
-									/>
-								)}
-								{data.loading && <span>Loading Data</span>}
-								{!data.loading && <span>New Scrape</span>}
-							</button>
-							<span>&nbsp;&nbsp;&nbsp;</span>
-							<select value={data.value} onChange={(event) => data._onSelect(event)}>
-								<option selected="selected">Previous Scrapes</option>
-								{data.values.map((item) => (
-									<option key={item} value={item}>
-										{new Date(item).toLocaleString()}
-									</option>
-								))}
-							</select>
-						</p>
-					</div>
-				</div>
-			</React.Fragment>
-		)}
-	</AppContext.Consumer>
-)
+  <AppContext.Consumer>
+    {data => (
+      <React.Fragment>
+        <div>
+          <div>
+            <br />
+            <Profile />
+          </div>
+          <div>
+            <p>
+              <button
+                className="button"
+                onClick={data.handleClickNewScrape}
+                disabled={data.loading}
+              >
+                {data.loading && (
+                  <i className="fas fa-sync" style={{ marginRight: "5px" }} />
+                )}
+                {data.loading && <span>Loading Data</span>}
+                {!data.loading && <span>New Scrape</span>}
+              </button>
+              <span>&nbsp;&nbsp;&nbsp;</span>
+              <select
+                value={data.value}
+                onChange={event => data._onSelect(event)}
+              >
+                <option selected="selected">Previous Scrapes</option>
+                {data.values.map(item => (
+                  <option key={item} value={item}>
+                    {new Date(item).toLocaleString()}
+                  </option>
+                ))}
+              </select>
+            </p>
+          </div>
+        </div>
+      </React.Fragment>
+    )}
+  </AppContext.Consumer>
+);
